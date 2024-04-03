f54c71720b8e74819be96cb4573fe560b6740ebb
Mohd Hamd <mdhsxsadar@gmail.com>
2024-04-03 15:24:03 +0530

    Calculator

---
+++ js.js
@@ -0,0 +1,23 @@
+let str="";
+let buttons=document.querySelectorAll(".btn");
+Array.from(buttons).forEach((btn)=>{
+    btn.addEventListener('click',(e)=>{
+        if(e.target.innerHTML == '='){
+            str=eval(str);
+            document.querySelector('input').value=str;
+        }
+        else if(e.target.innerHTML=='C'){
+            str='';
+            document.querySelector('input').value=str;
+
+        }
+
+        
+        else{
+        console.log(e.target);
+        str=str+e.target.innerHTML;
+        document.querySelector('input').value=str;
+        }
+
+    });
+});
